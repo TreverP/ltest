941fdf01c3b3172a7214e239dea0d4d7fd96bd53
Trever Pehrson <Trever.Pehrson@me.com>
2012-11-27 21:08:05 -0700

    new commit

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -2,36 +2,11 @@
 
 class Home_Controller extends Base_Controller {
 
-	//RESTful Controllers Setup
-
-	// public $restful = true;
-
-	// public function get_index()
-	// {
-	// 	# code...
-	// }
-
-	// public function post_index()
-	// {
-	// 	# code...
-	// }
-	//End of RESTful Controller Setup
 
 	public function action_index()
 	{
 		// return View::make('home.index');
 		echo "This is my index page!";
-	}
-
-	public function action_welcome($name, $place)
-	{
-		$data = array(
-			'name'  => $name,
-			'place' => $place );
-
-		//Creates and displays correct view
-		return View::make('welcome', $data);
-		// echo "Welcome to {$place}, {$name}!";
 	}
 
 	public function action_login()
@@ -44,5 +19,16 @@ class Home_Controller extends Base_Controller {
 	{
 		echo "This is the logout action";
 	}
+
+	// public function action_welcome($name, $place)
+	// {
+	// 	$data = array(
+	// 		'name'  => $name,
+	// 		'place' => $place );
+
+	// 	//Creates and displays correct view
+	// 	return View::make('welcome', $data);
+	// 	// echo "Welcome to {$place}, {$name}!";
+	// }
 
 }
